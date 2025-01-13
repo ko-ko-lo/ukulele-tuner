d62ce0017a19413bfba483f8a580675ec5598f9a
ko-ko-lo <d-kolodzey@web.de>
2025-01-13 18:28:04 +0100

    Added static modal for tuning options

---
+++ src/constants/tuningOptions.ts
@@ -0,0 +1,22 @@
+export const tuningOptions = [
+  {
+    id: "standard",
+    name: "Standard Tuning",
+    notes: ["G4", "C4", "E4", "A4"],
+  },
+  {
+    id: "low-g",
+    name: "Low G Tuning",
+    notes: ["G3", "C4", "E4", "A4"],
+  },
+  {
+    id: "baritone",
+    name: "Baritone Tuning",
+    notes: ["D3", "G3", "B3", "E4"],
+  },
+  {
+    id: "d-tuning",
+    name: "D Tuning",
+    notes: ["A4", "D4", "F#4", "B4"],
+  },
+];
--- src/pages/ManualTuner.tsx
+++ src/pages/ManualTuner.tsx
@@ -1,10 +1,41 @@
+import { useState } from "react";
+import Modal from "../components/Modal";
+import { tuningOptions } from "../constants/tuningOptions";
+import "../index.scss";
 import "../styles/variables.scss";
 
 const ManualTuner = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
     <div>
-      <h1>Manual Tuner</h1>
-      <p>This is the page for manual tuning.</p>
+      <h1>Press a button to guide your tuning.</h1>
+      <button id="secondary" onClick={() => setIsModalOpen(true)}>
+        Standard Tuning{" "}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          viewBox="0 0 16 16"
+          className="arrow-icon"
+        >
+          <path
+            fillRule="evenodd"
+            d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
+          />
+        </svg>
+      </button>
+      <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
+        <ul>
+          {tuningOptions.map((option) => (
+            <li key={option.id}>
+              <h3>{option.name}</h3>
+              <p>{option.notes.join("-")}</p>
+            </li>
+          ))}
+        </ul>
+      </Modal>
     </div>
   );
 };
