4b7ade130843890eb446497aac39ff098f08c631
ko-ko-lo <d-kolodzey@web.de>
2025-01-13 16:18:36 +0100

    Fixed Routing

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import "@fontsource/comfortaa/400.css";
 import "@fontsource/comfortaa/500.css";
 import "@fontsource/comfortaa/600.css";
 import "@fontsource/comfortaa/700.css";
-import { BrowserRouter as Router } from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import "./index.scss";
+import AutoTuner from "./pages/AutoTuner";
+import ManualTuner from "./pages/ManualTuner";
 import "./styles/variables.scss";
 
 /*function App() {
@@ -27,6 +29,10 @@ const App = () => {
   return (
     <Router>
       <Header /> {/* Header is always displayed */}
+      <Routes>
+        <Route path="/" element={<AutoTuner />} />
+        <Route path="/manual" element={<ManualTuner />} />
+      </Routes>
     </Router>
   );
 };
